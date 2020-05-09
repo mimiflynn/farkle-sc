fa1480053353db450d3a74875fe9ebf1a7732bd3
Mimi Flynn <mimiflynn@gmail.com>
2020-05-09 09:02:38 -0400

    need 500 to be on board warning

--- src/components/player-score.js
+++ src/components/player-score.js
@@ -10,12 +10,22 @@ export function PlayerScore(props) {
         select(player);
     }
 
+    function renderNotOnBoard() {
+        if (scorecard.total === 0) {
+            return (
+                <div className="alert alert-warning" role="alert">
+                    A score of 500 is needed in one roll to be on the board.
+                </div>
+            )
+        }
+    }
+
     return (
         <div className={classNames('card', {
-            'off-board': !scorecard.onBoard,
             'on-board': scorecard.onBoard
         })} key={player}>
             <div className="card-body">
+                {renderNotOnBoard()}
                 <h5 className="card-title">{player}</h5>
                 <p className="card-text">Total: {scorecard.total}</p>
                 <button
