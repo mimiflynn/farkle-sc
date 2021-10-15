7cb544d78ceef90af3114c416511a37dca4a4f55
Mimi Flynn <mimiflynn@gmail.com>
2021-10-15 19:23:41 -0400

    react app build with on board logic updates

--- react/src/components/game/player-score.js
+++ react/src/components/game/player-score.js
@@ -12,16 +12,6 @@ export function PlayerScore(props) {
                     A score of 500 is needed in one roll to be on the board.
                 </div>
             )
-        } else if (
-            scorecard.total >= 500 &&
-            (scorecard.turns.length === 1 ||
-                scorecard.turns[scorecard.turns.length - 2] === 0)
-        ) {
-            return (
-                <div className="alert alert-success" role="alert">
-                    You are now on the board!
-                </div>
-            )
         }
     }
 
