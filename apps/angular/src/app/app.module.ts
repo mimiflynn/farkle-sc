638863e8536c4ccb76566ae1b7a026068f45f764
Mimi <mimiflynn@gmail.com>
2023-02-21 09:51:02 -0500

    Start Scaffolding

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReferenceComponent } from './components/reference/reference.component';
+import { PlayComponent } from './pages/play/play.component';
+import { SetupComponent } from './pages/setup/setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReferenceComponent,
+    PlayComponent,
+    SetupComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
