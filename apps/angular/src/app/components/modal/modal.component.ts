54214086533a96ece912e87d26a5aa2071978f88
Mimi <mimiflynn@gmail.com>
2023-03-05 18:47:16 -0500

    routes

--- apps/angular/src/app/components/modal/modal.component.ts
+++ apps/angular/src/app/components/modal/modal.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'fsc-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent {
-
+  title = 'modal title';
 }
