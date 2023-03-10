9a31f652cd460c5ef95584e0ef4571c18b13109b
Mimi Flynn <mimiflynn@gmail.com>
2023-03-10 09:07:01 -0500

    convert add player component from react

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -24,7 +25,7 @@ import { PlayerComponent } from './components/player/player.component';
     AddPlayerComponent,
     PlayerComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/angular/src/app/components/add-player/add-player.component.ts
+++ apps/angular/src/app/components/add-player/add-player.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'fsc-add-player',
   templateUrl: './add-player.component.html',
-  styleUrls: ['./add-player.component.scss']
+  styleUrls: ['./add-player.component.scss'],
 })
 export class AddPlayerComponent {
+  @Output()
+  savePlayer = new EventEmitter<string>();
 
+  name: string = '';
+
+  constructor() {}
+
+  save(): void {
+    console.log('save name');
+    this.savePlayer.emit(this.name);
+  }
 }
